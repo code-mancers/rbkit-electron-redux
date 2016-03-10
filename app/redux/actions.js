f7ee247812eda6bec00d2684d27ba876f5329896
Revath S Kumar <rsk@revathskumar.com>
2016-03-10 18:00:33 +0530

    Using constants for action types

--- app/redux/actions.js
+++ app/redux/actions.js
@@ -1,16 +1,19 @@
+export const CONNECTED = 'CONNECTED';
+export const CONNECT_TO_SERVER = 'CONNECT_TO_SERVER';
+export const DISCONNECTED = 'DISCONNECTED';
+
 import RbkitConnection from '../rbkitConnection';
 
 export function connectToServer (ip) {
-  console.log('in connectToServer : ', ip);
   return dispatch => {
     RbkitConnection.connect({
       ip: ip,
       onConnect: () => {
         console.log('onConnect');
-        dispatch({type: 'CONNECTED'});
+        dispatch({type: CONNECTED});
       },
       onDisconnect: () => {
-        dispatch({type: 'DISCONNECTED'});
+        dispatch({type: DISCONNECTED});
       }
     });
   }
--- app/redux/reducers/connection.js
+++ app/redux/reducers/connection.js
@@ -1,14 +1,16 @@
+import {CONNECT_TO_SERVER, CONNECTED, DISCONNECTED} from './../actions'
+
 const initialState = {
   status: ''
 }
 
 const connection = function (state = initialState, action) {
   switch (action.type) {
-    case 'CONNECT_TO_SERVER':
+    case CONNECT_TO_SERVER:
       return Object.assign({}, state, { status: ''});
-    case 'CONNECTED':
+    case CONNECTED:
       return Object.assign({}, state, { status: 'CONNECTED'});
-    case 'DISCONNECTED':
+    case DISCONNECTED:
       return Object.assign({}, state, { status: 'DISCONNECTED'});
 
     default:
