029e7463b618f08a0c8d72ead2c3ed2149d81ddd
Revath S Kumar <rsk@revathskumar.com>
2016-03-11 15:13:22 +0530

    Initial redux zmq middleware

--- app/redux/actions.js
+++ app/redux/actions.js
@@ -1,23 +1,30 @@
 export const CONNECTED = 'CONNECTED';
 export const CONNECT_TO_SERVER = 'CONNECT_TO_SERVER';
 export const DISCONNECTED = 'DISCONNECTED';
+export const CONNECT = "CONNECT";
 
 import RbkitConnection from '../rbkitConnection';
 
 export function connectToServer (ip) {
-  return dispatch => {
-    RbkitConnection.connect({
-      ip: ip,
-      onConnect: () => {
-        console.log('onConnect');
-        dispatch({type: CONNECTED});
-      },
-      onDisconnect: () => {
-        dispatch({type: DISCONNECTED});
-      }
-    });
-  }
-};
+  return {
+    type: CONNECT,
+    sock: {
+      ip
+    }
+  };
+  // return dispatch => {
+  //   RbkitConnection.connect({
+  //     ip: ip,
+  //     onConnect: () => {
+  //       console.log('onConnect');
+  //       dispatch({type: CONNECTED});
+  //     },
+  //     onDisconnect: () => {
+  //       dispatch({type: DISCONNECTED});
+  //     }
+  //   });
+  // }
+}
 
 export function handshake (handshakeData) {
   console.log('in handshake : ', handshakeData);
--- app/redux/store.js
+++ app/redux/store.js
@@ -2,11 +2,50 @@ import { applyMiddleware, compose, createStore, combineReducers } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import * as reducers from './reducers/connection'
+import ZMQ from 'zmq';
+
+const requester = ZMQ.socket('req');
+requester.monitor(500, 0);
 
 let createStoreWithMiddleware;
 
+const zmqMiddlewareCreator = (zmq, options = {}) => {
+  console.log("zmqMiddlewareCreator");
+  const {onConnect, onDisconnect} = options;
+  return ({dispatch}) => {
+    return (next) => action => {
+      if (!action.sock) return next(action);
+      console.log('zmqMiddleWare :: ', action);
+
+      const {type, sock, ...rest} = action;
+      console.log('zmqMiddleWare :: ', sock, type, rest);
+
+      if (type) {
+        next({type, ...rest});
+      }
+
+      if (type === 'CONNECT') {
+        zmq.connect(`tcp://${sock.ip}:5556`);
+      }
+
+      zmq.on('connect', () => {
+        console.log('zmqMiddleWare :: on connect');
+        dispatch({
+          type: 'CONNECTED'
+        });
+      });
+
+      zmq.on('disconnect', () => {
+        dispatch({
+          type: 'DISCONNECTED'
+        });
+      });
+    };
+  };
+};
+
 createStoreWithMiddleware = compose(
-  applyMiddleware(logger(), thunk)
+  applyMiddleware(logger(), thunk, zmqMiddlewareCreator(requester))
 )(createStore)
 
 const rootReducer = combineReducers(reducers);
